b055aa688b0d74392665d4fb164415012d4e9cea
弥生 重松 <mirai3.2.1993@gmail.com>
2018-10-27 22:58:17 +0900

    Hacker News の GraphQL API を利用してニュース記事のリストを取得し、 React コンポーネントでレンダリングする

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    'gatsby-source-hacker-news',
     {
       resolve: 'gatsby-plugin-styled-components',
     },
--- src/pages/data.js
+++ src/pages/data.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-const Data = ({ data }) => (
-  <div>
-    <div>{data.site.siteMetadata.title}</div>
-    <div>{data.site.siteMetadata.description}</div>
-  </div>
-)
+// GraphQL のレスポンスが引数の props に入ってくる
+const Data = props => {
+  return (
+    <div>
+      {props.data.allHnStory.edges.map(edge => {
+        const story = edge.node
+
+        return (
+          <a style={{color: 'inherit'}} href={story.url} key={story.id}>
+            {story.title}
+            <small style={{display: 'block'}}>
+              {story.score} Point | {story.domain}
+            </small>
+          </a>
+        )
+      })}
+    </div>
+  )
+}
+
+// Hacker News GraphQL API
+// npm で gatsby-source-hacker-news パッケージをインストールする
+// https://www.gatsbyjs.org/packages/gatsby-source-hacker-news/?=gatsby-source-hacker-n
 export const query = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-        description
+    allHnStory {
+      edges {
+        node {
+          id
+          title
+          score
+          order
+          domain
+          url
+        }
       }
     }
   }
